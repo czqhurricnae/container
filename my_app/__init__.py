0b1f13ea388f0d64c64ec73620c5b47686818d84
c <7160844@qq.com>
2019-11-20 12:35:23 +0800

    新增 API: tools

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -59,7 +59,7 @@ def create_app():
     from .administ import admin_blueprint
     from .document import documents_blueprint
     from models import Tool, Project, Advise, Document, ProjectModelView, ToolModelView, DocumentModelView
-    from apis import ProjectAPI, SegmentationAPI
+    from apis import ProjectsAPI, SegmentationAPI, ToolsAPI
     app = Flask(__name__,
                 instance_path=INSTANCE_PATH,
                 static_folder=STATIC_PATH,
@@ -108,8 +108,9 @@ def create_app():
     bootstrap.init_app(app)
     # toolbar.init_app(app)
     api = Api(app)
-    api.add_resource(ProjectAPI, '/api/projects/')
+    api.add_resource(ProjectsAPI, '/api/projects/')
     api.add_resource(SegmentationAPI, '/api/segmentations/')
+    api.add_resource(ToolsAPI, '/api/tools/')
     return app
 
 
--- my_app/apis.py
+++ my_app/apis.py
@@ -1,17 +1,25 @@
-from flask_restful import Resource, fields, marshal_with, reqparse
-from models import Project
+from flask_restful import Resource, fields, marshal_with, marshal, reqparse
+from models import Project, Tool
 import jieba
+import json
 
-resource_fields = {
+projects_resource_fields = {
     'title': fields.String,
+}
+
+tools_resource_fields = {
+    'name': fields.String,
+    'size': fields.String,
+    'number': fields.Integer,
+    'description': fields.String,
 }
 
 parser = reqparse.RequestParser()
 parser.add_argument('search')
 
 
-class ProjectAPI(Resource):
-    @marshal_with(resource_fields)
+class ProjectsAPI(Resource):
+    @marshal_with(projects_resource_fields)
     def get(self, **kwargs):
         return [project for project in Project.query.all()]
 
@@ -19,5 +27,38 @@ class ProjectAPI(Resource):
 class SegmentationAPI(Resource):
     def post(self):
         args = parser.parse_args()
-        seg_list = jieba.cut(sentence=args['search'])
+        search = args.get('search', '')
+        seg_list = jieba.cut(sentence=search)
         return [seg for seg in seg_list]
+
+
+class ToolsAPI(Resource):
+    def post(self):
+        args = parser.parse_args()
+        search = args.get('search', '')
+        results = []
+
+        seg_list = jieba.cut(sentence=search)
+        ars_list = []
+        for seg in seg_list:
+            projects = Project.query.whoosh_search(seg, or_=True).all()
+            if len(projects) != 0:
+                ars_list.append(projects)
+        try:
+            projects_set = reduce(lambda x, y: set(x).intersection(set(y)),
+                                  ars_list)
+        except TypeError:
+            projects_set = []
+
+        if len(projects_set) != 0:
+            for project in projects_set:
+                project_tool = dict()
+                project_tool[u'project_id'] = project.id
+                project_tool[u'project_title'] = project.title
+                project_tool[u'tools'] = []
+                tools = Tool.query.filter_by(project_id=project.id).all()
+                for tool in tools:
+                    project_tool[u'tools'].append(
+                        marshal(vars(tool), tools_resource_fields))
+                results.append(project_tool)
+        return results
