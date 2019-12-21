587f531ceda93e263b51b0dd8e46fc93dc4d4a40
c <7160844@qq.com>
2019-12-21 18:27:14 +0800

    修改工时提交验证逻辑

--- my_app/index/views.py
+++ my_app/index/views.py
@@ -6,6 +6,7 @@ from functools import reduce
 import json
 import ast
 from datetime import datetime
+from my_app.errors import api_abort
 
 
 @index.route('/', methods=['GET', 'POST'])
@@ -67,20 +68,49 @@ def timesheets():
     from my_app.models.timesheet import Timesheet, db
     if request.method == 'POST':
         timesheets = request.get_json().get('timesheets')
+
         items = []
         for timesheet in timesheets:
-            items.append(
-                Timesheet(
-                    name=timesheet.get('name'),
-                    number=int(timesheet.get('number')),
-                    date=datetime.strptime(timesheet.get('date'), '%Y-%m-%d'),
-                    airplane=timesheet.get('airplane'),
-                    task=timesheet.get('task'),
-                    calculated_time=float(timesheet.get('calculatedTime')),
-                    completed=timesheet.get('completed', u'全部完成'),
-                    kind=timesheet.get('kind'),
-                    belongto_team=timesheet.get('belongto_team'),
-                    approved=timesheet.get('approved', u'否')))
-        db.session.add_all(items)
-        db.session.commit()
-    return 'timesheets commit successfully.'
+            name = timesheet.get('name', None)
+            number = timesheet.get('number', None)
+            date = timesheet.get('date', None)
+            airplane = timesheet.get('airplane', u'无')
+            task = timesheet.get('task', None)
+            calculatedTime = timesheet.get('calculatedTime', None)
+            completed = timesheet.get('completed', u'全部完成')
+            kind = timesheet.get('kind', None)
+            belongto_team = timesheet.get('belongto_team', None)
+            approved = timesheet.get('approved', u'否')
+
+            if calculatedTime == u'Infinity':
+                return api_abort(403,
+                                 u'你的工时里出现了 Infinity 代表无穷大的工时,这是不允许的,请联系管理员!')
+            else:
+                try:
+                    number = int(number)
+                    date = datetime.strptime(date, '%Y-%m-%d')
+                    calculated_time = float(calculatedTime)
+                except (UnicodeEncodeError, ValueError, TypeError) as e:
+                    return api_abort(400, e.args[0])
+
+                items.append(
+                    Timesheet(name=name,
+                              number=number,
+                              date=date,
+                              airplane=airplane,
+                              task=task,
+                              calculated_time=calculated_time,
+                              completed=completed,
+                              kind=kind,
+                              belongto_team=belongto_team,
+                              approved=approved))
+                try:
+                    db.session.add_all(items)
+                    db.session.commit()
+                    return jsonify({
+                        'message': 'Timesheets created.',
+                        'count': len(items)
+                    }), 201
+                except IntegrityError as e:
+                    db.session.rollback()
+                    return api_abort(409, e.args[0])
